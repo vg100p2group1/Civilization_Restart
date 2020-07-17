ab349e371d26bce6d8cfbe783c6c8fa3149418ca
INFCode <Wu_QiFei@sjtu.edu.cn>
2020-07-17 17:25:16 +0800

    add unlockChosen that can unlock the chosen skill in the current SkillSubSystem if it is not unlocked

--- src/Skill.elm
+++ src/Skill.elm
@@ -1,4 +1,5 @@
 module Skill exposing (SkillSystem, SkillSubSystem, Skill)
+import Css exposing (true)
 
 type alias SkillSystem = 
     { subsys : List SkillSubSystem
@@ -21,7 +22,7 @@ type alias Skill =
     }
 
 emptySkill : Skill
-emptySkill = Skill 0 0 False ""
+emptySkill = Skill -1 -1 False ""
 
 switchSubSystem : SkillSystem -> Int -> SkillSystem
 switchSubSystem sys dir =
@@ -45,3 +46,28 @@ choose sys (id, level) =
         skill = Maybe.withDefault emptySkill maybeSkill
     in
         {sys|chosen = (skill.id, skill.level), text = skill.desciption}
+
+
+-- return Nothing if the skill not exist (player currently choose nothing).
+-- return (inputSubsystem,0) back if the skill is already unlocked.
+-- return (newSubsystem, cost) where newSubsystem have the skill unlocked and 
+-- cost is the cost for this unlock if success
+unlockChosen : SkillSubSystem -> Maybe (SkillSubSystem, Int)
+unlockChosen sys = 
+    let 
+        (id, level) = sys.chosen
+        maybeSkill = getSkill sys (id, level)
+    in
+        case maybeSkill of 
+            Maybe.Nothing -> Maybe.Nothing
+            Just skill ->
+                if skill.unlocked then  -- the skill is already unlocked
+                    (sys, 0)
+                else  -- unlock the skill
+                let
+                    newSkill = {skill|unlocked = true}
+                    newSkills = List.filter (\sk -> not (sk.id == id && sk.level == level)) sys.skills :: skill
+                    cost = skill.level
+                    newSubsystem = {sys | skills = newSkills}
+                in
+                    (newSubsystem, cost)
