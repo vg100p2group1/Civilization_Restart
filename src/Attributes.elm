2331dec230d17eb53e964754a7de873ccf693986
INFCode <Wu_QiFei@sjtu.edu.cn>
2020-07-20 15:29:22 +0800

    add a getter to the Attribute

--- src/Attributes.elm
+++ src/Attributes.elm
@@ -1,6 +1,6 @@
-module Attributes exposing (PlayerAttr, AttrType(..))
+module Attributes exposing (PlayerAttr, AttrType(..), getAttr)
 
-import Dict exposing (Dict, fromList)
+import Dict exposing (Dict, get)
 
 -- it will only contain the attr that are adjusted
 -- NOTE: it is quire annoying that Dict only provide get function for comparable types,
@@ -14,3 +14,30 @@ type AttrType
     | Armor
     | Health
     | Clip      -- It acctually means total bullets but since the word Bullet is already used as a type, I chooce Clip instead
+
+attrTypeToInt : AttrType -> Int
+attrTypeToInt at = 
+    case at of 
+        Attack -> 0
+        Speed -> 1
+        Armor -> 2
+        Health -> 3
+        Clip -> 4
+
+getDefault : AttrType -> Int
+getDefault at =
+    case at of
+        Attack -> 10
+        Speed -> 10
+        Armor -> 50
+        Health -> 100
+        Clip -> 80
+
+getAttr : AttrType -> PlayerAttr -> Int
+getAttr t pAttr =
+    let
+        stored = get (attrTypeToInt t) pAttr
+    in
+    case stored of
+        Nothing -> getDefault t
+        Just val -> val
