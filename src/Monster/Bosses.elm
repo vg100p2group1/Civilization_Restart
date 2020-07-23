53b39383df747516eb3478e39ca9c1f6a392af8f
h6kplus <owenhuang17@outlook.com>
2020-07-23 17:10:24 +0800

    #853 init

---
+++ src/Monster/Bosses.elm
@@ -0,0 +1,9 @@
+module Monster.Boses exposing (allMonsterAct)
+
+import Shape exposing (Circle)
+import Map.Map exposing (Monster,MonsterType,Obstacle)
+import Model exposing (..)
+import Config exposing (bulletSpeed)
+import Weapon exposing (..)
+import Random exposing (..)
+
